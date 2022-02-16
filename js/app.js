e4b7ec2ad3ec1021e9826c763eae4a9d10678ca9
bdavijit <bdavijit@gmail.com>
2022-02-17 00:02:04 +0600

    error handling

--- js/app.js
+++ js/app.js
@@ -13,26 +13,62 @@ function ClearField(htmlID) {
   html_ID.value = "";
 }
 
+function FindError(htmlID){
+    let fieldValue = stringTOFloat(htmlID);
+    if(isNaN(fieldValue)){
+        alert(`${htmlID} is not a number.` );
+        return true;
+    }else if(fieldValue < 0 ){
+        alert(`${htmlID}: negative value not allowed` );
+        return true;
+    }
+    return false;
+}
+
 let Balance = 0;
 function ClickCalculate() {
-  let Expenses = stringTOFloat("Food") + stringTOFloat("Rent") + stringTOFloat("Clothes");
-  importByID("Total_Expenses").innerText = Expenses.toFixed(2);
-  Balance = stringTOFloat("Income") - Expenses;
-  importByID("Balance").innerText = Balance.toFixed(2);
-  //
-  //   ClearField("Income");
-  ClearField("Food");
-  ClearField("Rent");
-  ClearField("Clothes");
+
+   if(FindError("Income")){
+
+   }else if(FindError("Food")){
+       
+   }
+   else if(FindError("Rent")){
+
+   }
+   else if(FindError("Clothes")){
+
+   }else{
+    let Expenses = stringTOFloat("Food") + stringTOFloat("Rent") + stringTOFloat("Clothes");
+    importByID("Total_Expenses").innerText = Expenses.toFixed(2);
+    Balance = stringTOFloat("Income") - Expenses;
+    importByID("Balance").innerText = Balance.toFixed(2);
+    //
+    //   ClearField("Income");
+    ClearField("Food");
+    ClearField("Rent");
+    ClearField("Clothes");
+   }
+
+
 }
 
 function ClickSave() {
-  let SavingAmount = stringTOFloat("Income") * (stringTOFloat("Save") / 100);
-  let RemainingBalance = Balance - SavingAmount;
-//   console.log(stringTOFloat("Balance"));
-//   console.log(SavingAmount);
-  importByID("Saving_Amount").innerText = SavingAmount;
-  importByID("Remaining_Balance").innerText = RemainingBalance;
+    if(FindError("Save")){
 
-  ClearField("Save");
+    }else{
+        let SavingAmount = stringTOFloat("Income") * (stringTOFloat("Save") / 100);
+        let RemainingBalance = Balance - SavingAmount;
+        importByID("Saving_Amount").innerText = SavingAmount.toFixed(2);
+        //Error
+        if(RemainingBalance < 0){
+           alert("Insufficient Balance")
+        }else{
+          importByID("Remaining_Balance").innerText = RemainingBalance.toFixed(2);
+        
+          ClearField("Save");
+        }
+    }
+
+
 }
